c9f64a0ea7a22f7de1bec2a8592f94f43e401390
Renis Makadia <renmak@gmail.com>
2021-05-24 23:40:03 -0700

    two module, two urls

---
+++ src/app/mod-one/mod-one.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentAComponent } from './component-a/component-a.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComponentAComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModOneModule { }
